4e90e1275e7d59314c30259db4534f198b69e6d2
sneha12199 <khoklesneha5@gmail.com>
2023-08-28 15:12:56 +0530

    ok

--- If.java
+++ If.java
@@ -2,8 +2,10 @@
  class Main{
     public static void main(String[] args){
       
-        int age=2;
-        if  (age>18){
+        // int age=207;
+        // if  (age>18){
+            int x=3;
+            if(x % 2==0){
             System.out.println("aligible for marraige");
             
         }else {
